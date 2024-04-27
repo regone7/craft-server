75df26f82e2f3b39f370e7e88e3a16ea6539a830
regone7 <regone12345@gmail.com>
2024-04-28 02:17:57 +0600

    for data

--- index.js
+++ index.js
@@ -26,6 +26,11 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const craftCollection = client.db("craftDB").collection("craft");
+
+    app.get('/craft',async(req,res)=>{
+      const cursor = craftCollection.find();
+      const result = await cursor.toArray();
+    })
     app.post('/craft',async(req, res)=>{
       const infoCraft=req.body;
       console.log(infoCraft)
