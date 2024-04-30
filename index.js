50d9cfc8b35fb9f3cd3d692a942c1c7ba3961dca
regone7 <regone12345@gmail.com>
2024-04-30 20:49:51 +0600

    somes bugs fail

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://art-craft-b287c.web.app',
+  ]
+}));
 app.use(express.json());
 
 
@@ -24,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const craftCollection = client.db("craftDB").collection("craft");
     const newCollection = client.db("craftDB").collection("categories");
     // data show
@@ -107,7 +112,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
